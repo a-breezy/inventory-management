72fdb426ae2c37a164b817e3040592f2516fd080
Ambrose Wilkinson <ambrose.wilkins@gmail.com>
2024-08-07 12:53:05 -0400

    working on env"

--- firebase.js
+++ firebase.js
@@ -2,15 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-console.log("firebase config", {
-  apiKey: process.env.NEXT_PUBLIC_INVENTORY_MANAGEMENT_apiKey,
-  authDomain: process.env.NEXT_PUBLIC_INVENTORY_MANAGEMENT_authDomain,
-  projectId: process.env.NEXT_PUBLIC_INVENTORY_MANAGEMENT_projectId,
-  storageBucket: process.env.NEXT_PUBLIC_INVENTORY_MANAGEMENT_storageBucket,
-  messagingSenderId:
-    process.env.NEXT_PUBLIC_INVENTORY_MANAGEMENT_messagingSenderId,
-  appId: process.env.NEXT_PUBLIC_INVENTORY_MANAGEMENT_appId,
-});
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_INVENTORY_MANAGEMENT_apiKey,
   authDomain: process.env.NEXT_PUBLIC_INVENTORY_MANAGEMENT_authDomain,
